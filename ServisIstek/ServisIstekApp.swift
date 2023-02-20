8bb5ba00ef0f18037fc70f21ba84b4ac09e96a1a
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-20 20:25:01 +0300

    Initial Commit

---
+++ ServisIstek/ServisIstekApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServisIstekApp.swift
+//  ServisIstek
+//
+//  Created by Ege Aydemir on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ServisIstekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
