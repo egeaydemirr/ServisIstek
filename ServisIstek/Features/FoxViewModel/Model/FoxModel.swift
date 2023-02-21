74bfd02141c309fc50c95b16a37632b0185534c6
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-21 14:55:10 +0300

    Model

---
+++ ServisIstek/Features/FoxViewModel/Model/FoxModel.swift
@@ -0,0 +1,15 @@
+//
+//  FoxModel.swift
+//  ServisIstek
+//
+//  Created by Ege Aydemir on 21.02.2023.
+//
+
+
+import Foundation
+
+// MARK: - FoxModel
+struct FoxModel: Codable {
+    let image: String?
+    let link: String?
+}
